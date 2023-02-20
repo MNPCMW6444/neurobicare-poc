885aab2f2506c046dd8e73e45ea7fc488d2e94f7
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2023-02-20 12:48:18 +0200

    moved to personal neurobicare-ai

--- src/comps/util/NeurobicareColors.tsx
+++
@@ -1,8 +0,0 @@
-enum NeurobicareColors {
-  p0 = "#492e7b",
-  p1 = "#71589e",
-  p2 = "#917aba",
-  p3 = "#cab7ec",
-  p4 = "#eae4f5",
-}
-export default NeurobicareColors;
--- src/hooks/clear.ts
+++
@@ -1,9 +0,0 @@
-import { useDispatch } from "react-redux";
-import { clear } from "../store/reducers/museReducer";
-
-const useClear = () => {
-  const dispatch = useDispatch();
-  return () => dispatch(clear());
-};
-
-export default useClear;
